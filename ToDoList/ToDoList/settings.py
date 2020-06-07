8092e881b90d68ceb7be987f0777c1694cd7c10d
swankhede <31096252+swankhede@users.noreply.github.com>
2020-06-07 12:20:46 +0530

    Update settings.py

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DIR = os.path.join(BASE_DIR,'template')
-STATICDIR = os.path.join(BASE_DIR,'static')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICDIRS = [
-    STATICDIR
+STATICFILES_DIRS = [
+   STATIC_DIR
 ]
